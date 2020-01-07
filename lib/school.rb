7be1be838912676517fc11715316be70785967b6
Eddie Hernandez <mchh.004@gmail.com>
2020-01-07 20:12:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :name 
+  
+  def initializer(name, roster)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+    def add_student(name, grade)
+      @name, @roster[:grade] << roster
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+  
+end
